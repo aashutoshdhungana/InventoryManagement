a5b7138885a49a2568d58becf16a05f4f56ba24b
Kirtee011 <kirteemaharjan123@gmail.com>
2020-11-22 16:06:57 +0545

    rough sketch for checkout.c

--- checkout.c
+++ checkout.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+#define MAX_SIZE 100
 //grocery item for each customer
 typedef struct gItem
 {
@@ -9,24 +11,56 @@ typedef struct gItem
 } gItem;
 
 //Checkout
-typedef struct checkout
+typedef struct customer_detail
 {
-    char name[100];
+    char name[50];
     float cash;
-    int (*grocery_list)[2];
-} checkout;
+    int (*grocery_list)[2]; //int grocery_list[MAX_SIZE][2];
+} customer_detail;
 
 typedef struct queue
 {
-    struct checkout customer_data;
+    struct customer_detail data;
     struct queue *next;
 } queue;
+
+queue *rear = NULL, *front = NULL;
+
+// to enqueue the queue
+void enqueue(customer_detail *new_data);
+
+// dequeue the queue
+customer_detail *dequeue(void);
+
+// calculate total for customer
+float total(customer_detail customer)
+{
+}
+
+//print the log
+void print_log(customer_detail customer, bool has_enough_money)
+{
+    /* format :
+        Customer - Karen
+
+        Apples x3 @ $0.99
+        Swiss Cheese x1 @ $2.49
+
+        Total: $6.48
+        Thank you, come back soon!
+        (Customer did not have enough money and was REJECTED
+        Thank you, come back soon!)-> if has_enough_money = FALSE;
+        -------------------------------------------------------
+    */
+}
 
 int main(void)
 {
     FILE *fptr = NULL;
     char *customer = NULL;
 
+    // input a file to read the queue??
+    // here test.txt is useed
     fptr = fopen("test.txt", "r");
 
     if (fptr == NULL)
@@ -35,8 +69,19 @@ int main(void)
         exit(1);
     }
 
-    //loop throuh file
+    //repeat until eof
     //read file line by line
+    //insert data into customer_detail struct
+    //enqueue the data
+
+    //dequeue the data untill rear
+    //calculate total
+    //total < cash :
+    //print_log
+    //total >= cash
+    //deduct from inventory
+    //check for if restock required
+    //inventory message
 
     fclose(fptr);
     return 0;
